bbc8f87fdb11d22583abbf3312af1fd6f258d87e
zackmorgs <4538979+zackmorgs@users.noreply.github.com>
2024-03-07 08:46:17 -0600

    updated missing dependency

--- src/components/wallet-providers.tsx
+++ src/components/wallet-providers.tsx
@@ -2,6 +2,22 @@ import { PROVIDER_ID, Provider, useWallet } from "@txnlab/use-wallet"
 import { Button } from "./button";
 import { TitleMd } from "./title";
 import Image from "next/image";
+interface AccountSelectProps {
+    provider: Provider;
+    activeAccount?: any;
+  }
+const AccountSelect = ({ provider, activeAccount }: AccountSelectProps) => (
+    <select value={activeAccount ? activeAccount.address : "Address"} onChange={(e) => provider.setActiveAccount(e.target.value)} className="w-max-sm">
+        <option value={"Address"}>
+            Address
+        </option>
+      {provider.accounts.map((account) => (
+        <option key={"account-" + account.address} value={account.address}>
+          {account.address}
+        </option>
+      ))}
+    </select>
+  );
 
 export default function WalletProviders() {
     const { providers, activeAccount } = useWallet();
@@ -14,12 +30,12 @@ export default function WalletProviders() {
                 <ul id="wallets_list" className="list-none pt-3">
                     {providers?.map((provider, index) => {
                         if (anyConnected && activeAccount) {
+                            console.log("provider:", provider.metadata.name, "active:", activeAccount.providerId )
                             if (provider.metadata.name.toLocaleLowerCase() == activeAccount.providerId) {
                                 return (
                                     <section id="wallet_connected">
                                         <TitleMd>Connected to {activeAccount?.name}</TitleMd>
-                                        <p>{activeAccount?.address}</p>
-                                        <p>{activeAccount?.email}</p>
+                                        <AccountSelect provider={provider} activeAccount={activeAccount}/>
                                         <Button label={`Disconnect from ${provider.metadata.name}`} onClick={provider.disconnect}>
                                             Disconnect from {provider.metadata.name}
                                         </Button>                                
