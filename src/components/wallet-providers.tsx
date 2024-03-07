f78c78badfc87dc1f9fd611bd9996334bdc02096
zackmorgs <4538979+zackmorgs@users.noreply.github.com>
2024-03-07 10:25:09 -0600

    added content to connected wallet state

--- src/components/wallet-providers.tsx
+++ src/components/wallet-providers.tsx
@@ -7,17 +7,22 @@ interface AccountSelectProps {
     activeAccount?: any;
 }
 const AccountSelect = ({ provider, activeAccount }: AccountSelectProps) => (
-    <select value={activeAccount ? activeAccount.address : "Address"} onChange={(e) => provider.setActiveAccount(e.target.value)}
-        className="border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        <option value={""}>
-            --Please Choose an Address--
-        </option>
-        {provider.accounts.map((account, index) => (
-            <option key={index} value={account.address}>
-                {account.address}
+    <>
+        <label htmlFor="account_select" className="font-semibold">
+            Select Account
+        </label>
+        <select id="account_select" value={activeAccount ? activeAccount.address : "Address"} onChange={(e) => provider.setActiveAccount(e.target.value)}
+            className="border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value={""}>
+                --Please Choose an Address--
             </option>
-        ))}
-    </select>
+            {provider.accounts.map((account, index) => (
+                <option key={index} value={account.address}>
+                    {account.address}
+                </option>
+            ))}
+        </select>
+    </>
 );
 
 export default function WalletProviders() {
@@ -25,50 +30,56 @@ export default function WalletProviders() {
     const anyConnected = providers?.some(provider => provider.isConnected);
 
     return (
-        <section id="wallet_section" className="block mx-auto px-4 sm:px-6 py-4">
-            <nav id="wallet_nav" className="max-w-sm mx-auto mt-4">
-                {!anyConnected && <p className="text-center text-lg">Please connect a wallet.</p>}
-                <ul id="wallets_list" className="list-none pt-3 max-w-sm">
-                    {providers?.map((provider, index) => {
-                        if (anyConnected && activeAccount) {
-                            console.log("provider:", provider.metadata.name, "active:", activeAccount.providerId)
-                            if (provider.metadata.name.toLocaleLowerCase() == activeAccount.providerId) {
-                                return (
-                                    <section id="wallet_connected">
-                                        <span className="text-center block mb-2">
-                                            <TitleMd>Connected to {provider.metadata.name}</TitleMd>
-                                        </span>
-                                        <u>Stats for Nerds:</u>
-                                        <div>
-                                            <b>Name: </b>
-                                            <span>
-                                                {activeAccount.name}
-                                            </span>
-                                        </div>
-                                        <div>
-                                            <b>ProviderId: </b><span>{activeAccount.providerId}</span>
-                                        </div>
 
-                                        <AccountSelect provider={provider} activeAccount={activeAccount} />
-                                        <Button label={`Disconnect from ${provider.metadata.name}`} onClick={provider.disconnect}>
-                                            Disconnect from {provider.metadata.name}
+        <>
+            <section id="wallet_section" className="block mx-auto px-4 sm:px-6 py-4">
+                <nav id="wallet_nav" className="max-w-sm mx-auto mt-4">
+                    {!anyConnected && <p className="text-center text-lg">Please connect a wallet.</p>}
+                    <ul id="wallets_list" className="list-none pt-3 max-w-sm">
+                        {providers?.map((provider, index) => {
+                            if (anyConnected && activeAccount) {
+                                // console.log("provider:", provider.metadata.name, "active:", activeAccount.providerId)
+                                if (provider.metadata.name.toLocaleLowerCase() == activeAccount.providerId) {
+                                    return (
+                                        <section id="wallet_connected">
+                                            <span className="text-center block mb-2">
+                                                <TitleMd>Connected to {provider.metadata.name}</TitleMd>
+                                            </span>
+                                            <u>Stats for Nerds:</u>
+                                            <div>
+                                                <b>Name: </b>
+                                                <span>
+                                                    {activeAccount.name}
+                                                </span>
+                                            </div>
+                                            <div>
+                                                <b>ProviderId: </b><span>{activeAccount.providerId}</span>
+                                            </div>
+                                            <br/>
+                                            <AccountSelect provider={provider} activeAccount={activeAccount} />
+                                            <br />
+                                            <hr />
+                                            <br />
+                                            <Button label={`Disconnect from ${provider.metadata.name}`} onClick={provider.disconnect}>
+                                                Disconnect from {provider.metadata.name}
+                                            </Button>
+                                        </section>
+                                    )
+                                }
+                            } else {
+                                return (
+                                    <li key={provider.metadata.id}>
+                                        <Button onClick={provider.connect} label={`Connect to ${provider.metadata.name}`}>
+                                            <Image src={provider.metadata.icon} width={24} height={24} className="inline-block relative" alt="Connect to Daffi" /> {provider.metadata.name}
                                         </Button>
-                                    </section>
+                                    </li>
                                 )
                             }
-                        } else {
-                            return (
-                                <li key={provider.metadata.id}>
-                                    <Button onClick={provider.connect} label={`Connect to ${provider.metadata.name}`}>
-                                        <Image src={provider.metadata.icon} width={24} height={24} className="inline-block relative" alt="Connect to Daffi" /> {provider.metadata.name}
-                                    </Button>
-                                </li>
-                            )
-                        }
-                    })}
-                </ul>
-            </nav>
-        </section>
+                        })}
+                    </ul>
+                </nav>
+            </section>
+        </>
 
     )
 
