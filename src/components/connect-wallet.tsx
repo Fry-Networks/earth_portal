b49186fd332f7aba817c2f460407c1b6a73c436c
zackmorgs <4538979+zackmorgs@users.noreply.github.com>
2024-03-07 08:57:51 -0600

    attempted to make wallet connect work will need new project id

--- src/components/connect-wallet.tsx
+++ src/components/connect-wallet.tsx
@@ -33,7 +33,7 @@ export default function ConnectWallet() {
               metadata: {
                 name: "Fry Foundation",
                 description: "Authenticate yourself",
-                url: "https://weather.fryfoundation.com",
+                url: "https://soil.fryfoundation.com",
                 icons: ["https://walletconnect.com/walletconnect-logo.png"],
               },
             },
--- src/components/wallet-providers.tsx
+++ src/components/wallet-providers.tsx
@@ -5,19 +5,20 @@ import Image from "next/image";
 interface AccountSelectProps {
     provider: Provider;
     activeAccount?: any;
-  }
+}
 const AccountSelect = ({ provider, activeAccount }: AccountSelectProps) => (
-    <select value={activeAccount ? activeAccount.address : "Address"} onChange={(e) => provider.setActiveAccount(e.target.value)} className="w-max-sm">
-        <option value={"Address"}>
-            Address
+    <select value={activeAccount ? activeAccount.address : "Address"} onChange={(e) => provider.setActiveAccount(e.target.value)} 
+    className="border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <option value={""}>
+            --Please Choose an Address--
         </option>
-      {provider.accounts.map((account) => (
-        <option key={"account-" + account.address} value={account.address}>
-          {account.address}
-        </option>
-      ))}
+        {provider.accounts.map((account, index) => (
+            <option key={index} value={account.address}>
+                {account.address}
+            </option>
+        ))}
     </select>
-  );
+);
 
 export default function WalletProviders() {
     const { providers, activeAccount } = useWallet();
@@ -27,18 +28,18 @@ export default function WalletProviders() {
         <section id="wallet_section" className="block mx-auto px-4 sm:px-6 py-4">
             <nav id="wallet_nav" className="max-w-sm mx-auto mt-4">
                 <p className="text-center">Please connect a wallet.</p>
-                <ul id="wallets_list" className="list-none pt-3">
+                <ul id="wallets_list" className="list-none pt-3 max-w-sm">
                     {providers?.map((provider, index) => {
                         if (anyConnected && activeAccount) {
-                            console.log("provider:", provider.metadata.name, "active:", activeAccount.providerId )
+                            console.log("provider:", provider.metadata.name, "active:", activeAccount.providerId)
                             if (provider.metadata.name.toLocaleLowerCase() == activeAccount.providerId) {
                                 return (
                                     <section id="wallet_connected">
-                                        <TitleMd>Connected to {activeAccount?.name}</TitleMd>
-                                        <AccountSelect provider={provider} activeAccount={activeAccount}/>
+                                        <TitleMd>Connected to {provider.metadata.name}</TitleMd>
+                                        <AccountSelect provider={provider} activeAccount={activeAccount} />
                                         <Button label={`Disconnect from ${provider.metadata.name}`} onClick={provider.disconnect}>
                                             Disconnect from {provider.metadata.name}
-                                        </Button>                                
+                                        </Button>
                                     </section>
                                 )
                             }
