62315c266d7226307baff5cc8e6cb6c64ddf9ace
zackmorgs <4538979+zackmorgs@users.noreply.github.com>
2024-03-12 15:15:42 -0500

    attempte to change favicon, removed facebook link from footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -34,11 +34,11 @@ export default function Footer() {
                             </g>
                         </svg>
                     </Link>
-                    <Link className="p-2" href="https://facebook.com/your-profile" target="_blank">
+                    {/* <Link className="p-2" href="https://facebook.com/your-profile" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill="white" height="20px" width="20px" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="73 0 267 266.9" enableBackground="new 73 0 267 266.9" xmlSpace="preserve"><script id="dashlane_tiresias" /><script id="dashlane_webauthn" />
                             <path id="f" fill="white" d="M255.4,262.3v-99.8h33.5l5-38.9h-38.5V98.8c0-11.3,3.1-18.9,19.3-18.9l20.6,0V45  c-3.6-0.5-15.8-1.5-30-1.5c-29.7,0-50,18.1-50,51.4v28.7h-33.6v38.9h33.6v99.8H255.4z" />
                         </svg>
-                    </Link>
+                    </Link> */}
                     <Link className="p-2" href="https://www.youtube.com/channel/UCXwYRxNo3yOmQjq1eEClVjg" target="_blank">
                         <Image
                             alt="Youtube"
